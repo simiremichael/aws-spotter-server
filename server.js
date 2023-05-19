6868b9b7ade25f25723db69184a524390dbc04fa
Michael Simire <simiremichael@gmail.com>
2023-05-19 09:10:46 +0100

    updated

--- server.js
+++ server.js
@@ -21,18 +21,23 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cookieParser())
 
- app.use(cors())
+ app.use(cors({
+    credentials: true,
+    origin: ['http://13.245.161.92', 'http://localhost:3000', 
+   //'https://www.residencespotter.com'
+    ]
+  }))
 
-// app.use( async(req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://my-property-finder.vercel.app");
-//   res.header("Access-Control-Allow-Headers", "https://my-property-finder.vercel.app")
-//   res.header("Access-Control-Allow-Credentials", true );
-//   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-//     return res.status(200).json({});
-//   }
-//     next();
-// }) 
+app.use( async(req, res, next) => {
+  res.header("Access-Control-Allow-Origin", 'http://13.245.161.92');
+  res.header("Access-Control-Allow-Headers", 'http://13.245.161.92')
+  res.header("Access-Control-Allow-Credentials", true );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+    next();
+}) 
 
 app.use('/api/properties', propertyRoute);
 app.use('/api/users', userRoute);
