8ecd3877031b5be41f4e019bf71f90eca3c19e9e
Michael Simire <simiremichael@gmail.com>
2023-05-19 08:54:32 +0100

    updated

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ app.use('/api/propertyscouting', scoutingRoute);
 app.use('/api/events', eventRoute);
 app.use('/api/save', saveRoute);
 
-const PORT = process.env.PORT || 5000;
-// 'mongodb://localhost:27017/property' 
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const PORT = process.env.PORT || 5000; 
+// 'mongodb://localhost:27017/property' process.env.MONGODB_URL 
+mongoose.connect("mongodb://localhost:27017/property", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
