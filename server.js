4b460c7fd4d0019bd4a171a81d429e64faf1f5b6
Michael Simire <simiremichael@gmail.com>
2023-05-19 09:08:30 +0100

    updated

--- server.js
+++ server.js
@@ -21,12 +21,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cookieParser())
 
- app.use(cors({
-  credentials: true,
-  origin: ['http://13.245.161.92', 'http://localhost:3000', 
- //'https://www.residencespotter.com'
-  ]
-}))
+ app.use(cors())
 
 // app.use( async(req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "https://my-property-finder.vercel.app");
