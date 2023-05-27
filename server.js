eb5db3f4009ac1c149dafb3d53520ec339e9b989
Michael Simire <simiremichael@gmail.com>
2023-05-27 14:10:15 +0100

    updated

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cookieParser())
 
  app.use(cors({
     credentials: true,
-    origin: ['http://13.245.161.92', 'http://localhost:3000', 
+    origin: ['http://13.245.249.148', 'http://localhost:3000', 
    'https://www.residencespotter.com'
     ],
     headers: 'x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization, userauthorization,userAuthorization, comauthorization, comAuthorization, *'
